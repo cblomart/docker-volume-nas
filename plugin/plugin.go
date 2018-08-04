c7377d39b97d9d75e1ed4d68c8a4ae4805b4282f
Cédric Blomart <cblomart@gmail.com>
2018-08-04 13:26:48 +0200

    skeleton

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -1,5 +1,9 @@
 package plugin
 
+import (
+	"github.com/docker/go-plugins-helpers/volume"
+)
+
 const (
 	// Name is the plugin name
 	Name = "volume-nas"
@@ -13,4 +17,44 @@ type Nas struct {
 // Name returns plugin name
 func (p *Nas) Name() string {
 	return Name
+}
+
+// Create creates a new volume in the mount point
+func (p *Nas) Create(*volume.CreateRequest) error {
+
+}
+
+// List lists volumes in the mount point
+func (p *Nas) List() (*volume.ListResponse, error) {
+
+}
+
+// Get gets a specific volume
+func (p *Nas) Get(*volume.GetRequest) (*volume.GetResponse, error) {
+
+}
+
+// Remove removes a volume from the mount point
+func (p *Nas) Remove(*volume.RemoveRequest) error {
+
+}
+
+// Path returns the path with the mount point
+func (p *Nas) Path(*volume.PathRequest) (*PathResponse, error) {
+
+}
+
+// Mount does nothing as the mount point should already be mounted
+func (p *Nas) Mount(*volume.MountRequest) (*volume.MountResponse, error) {
+
+}
+
+// Unmout does nothing as the mount point should already be mounted
+func (p *Nas) Unmount(*volume.UnmountRequest) error {
+
+}
+
+// Returns Capabilities of the module
+func (p *Nas) Capabilities() *volume.CapabilitiesResponse {
+
 }
