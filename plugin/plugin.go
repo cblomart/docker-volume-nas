a57fb9ab9a327856026e41f042017ef76961ca09
Cédric Blomart <cblomart@gmail.com>
2018-08-05 20:48:20 +0200

    revert to pointers

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -24,19 +24,19 @@ type Nas struct {
 }
 
 // Name returns plugin name
-func (p Nas) Name() string {
+func (p *Nas) Name() string {
 	return Name
 }
 
 // GetMountPoint returns sanitized mount point
-func (p Nas) GetMountPoint() string {
+func (p *Nas) GetMountPoint() string {
 	path := strings.Replace(p.MountPoint, "//", "/", -1)
 	path = strings.TrimRight(path, "/")
 	return path
 }
 
 // CheckVolumePath checks the volume path
-func (p Nas) CheckVolumePath(name string) (string, error) {
+func (p *Nas) CheckVolumePath(name string) (string, error) {
 	path := fmt.Sprintf("%s/%s", p.GetMountPoint(), name)
 	info, err := os.Stat(path)
 	if err != nil {
@@ -49,7 +49,7 @@ func (p Nas) CheckVolumePath(name string) (string, error) {
 }
 
 // Create creates a new volume in the mount point
-func (p Nas) Create(request volume.CreateRequest) error {
+func (p *Nas) Create(request volume.CreateRequest) error {
 	log.Printf("%s create volume %s\n", Name, request.Name)
 	path := fmt.Sprintf("%s/%s", p.GetMountPoint(), request.Name)
 	info, err := os.Stat(path)
@@ -85,7 +85,7 @@ func (p Nas) Create(request volume.CreateRequest) error {
 }
 
 // List lists volumes in the mount point
-func (p Nas) List() (volume.ListResponse, error) {
+func (p *Nas) List() (volume.ListResponse, error) {
 	log.Printf("%s list volumes\n", Name)
 	infos, err := ioutil.ReadDir(p.GetMountPoint())
 	if err != nil {
@@ -118,7 +118,7 @@ func (p Nas) List() (volume.ListResponse, error) {
 }
 
 // Get gets a specific volume
-func (p Nas) Get(request volume.GetRequest) (volume.GetResponse, error) {
+func (p *Nas) Get(request volume.GetRequest) (volume.GetResponse, error) {
 	log.Printf("%s get volume %s\n", Name, request.Name)
 	path, err := p.CheckVolumePath(request.Name)
 	if err != nil {
@@ -137,7 +137,7 @@ func (p Nas) Get(request volume.GetRequest) (volume.GetResponse, error) {
 }
 
 // Remove removes a volume from the mount point
-func (p Nas) Remove(request volume.RemoveRequest) error {
+func (p *Nas) Remove(request *volume.RemoveRequest) error {
 	log.Printf("%s remove volume %s\n", Name, request.Name)
 	path, err := p.CheckVolumePath(request.Name)
 	if err != nil {
@@ -148,39 +148,39 @@ func (p Nas) Remove(request volume.RemoveRequest) error {
 }
 
 // Path returns the path with the mount point
-func (p Nas) Path(request volume.PathRequest) (volume.PathResponse, error) {
+func (p *Nas) Path(request *volume.PathRequest) (*volume.PathResponse, error) {
 	log.Printf("%s volume path  %s\n", Name, request.Name)
 	path, err := p.CheckVolumePath(request.Name)
 	if err != nil {
 		log.Printf("Could not check path for %s: %s\n", request.Name, err)
-		return volume.PathResponse{}, err
+		return nil, err
 	}
 	response := volume.PathResponse{
 		Mountpoint: path,
 	}
 	p.verbose("Returning path")
 	p.dump(response)
-	return response, nil
+	return &response, nil
 }
 
 // Mount does nothing as the mount point should already be mounted
-func (p Nas) Mount(request volume.MountRequest) (volume.MountResponse, error) {
+func (p *Nas) Mount(request *volume.MountRequest) (*volume.MountResponse, error) {
 	log.Printf("%s mount volume %s\n", Name, request.Name)
 	path, err := p.CheckVolumePath(request.Name)
 	if err != nil {
 		log.Printf("Could not get path for %s: %s", request.Name, err)
-		return volume.MountResponse{}, err
+		return nil, err
 	}
 	response := volume.MountResponse{
 		Mountpoint: path,
 	}
 	p.verbose("Mount volume:")
 	p.dump(response)
-	return response, nil
+	return &response, nil
 }
 
 // Unmount does nothing as the mount point should already be mounted
-func (p Nas) Unmount(request volume.UnmountRequest) error {
+func (p *Nas) Unmount(request *volume.UnmountRequest) error {
 	log.Printf("%s unmount volume %s\n", Name, request.Name)
 	_, err := p.CheckVolumePath(request.Name)
 	if err != nil {
@@ -191,7 +191,7 @@ func (p Nas) Unmount(request volume.UnmountRequest) error {
 }
 
 // Capabilities of the module
-func (p Nas) Capabilities() *volume.CapabilitiesResponse {
+func (p *Nas) Capabilities() *volume.CapabilitiesResponse {
 	log.Printf("%s capabilities\n", Name)
 	return &volume.CapabilitiesResponse{
 		Capabilities: volume.Capability{
